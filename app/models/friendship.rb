de24e475788afbb039fc79e22c2723597c030dc3
Anne-Charlotte Morizot <annecharlotte.morizot@gmail.com>
2022-01-11 18:22:45 +0100

    friendship : add scope and user : add relationals, add seeds

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,4 +1,6 @@
 class Friendship < ApplicationRecord
   belongs_to :user
   belongs_to :friend, foreign_key: :friend_id, class_name: 'User'
+
+  scope :confirmed, -> { where(is_confirmed: true) }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :memberships
-  has_many :dreams, through: :memberships
   has_many :owned_dreams, foreign_key: :owner_id, class_name: 'Dream'
+
+  has_many :memberships
+  has_many :dreams, through: :memberships # where memberships.status = accepted..
+
   has_many :friendships
-  has_many :friends, -> { where is_confirmed: true }, through: :friendships, class_name: 'User'
+  # has_many :confirmed_friendships, -> { Friendship.confirmed }, through: :friendships
+
+  # has_many :friends, through: :confirmed_friendships, class_name: 'User'
+  # has_many :friends, , through: :friendships, class_name: 'User'
+  has_many :friends, -> { Friendship.confirmed }, through: :friendships
 end
